68b5a3b0b1956ddcfa8ff6761b2d0883e942a59f
enockgand <enockgand@gmail.com>
2023-03-23 06:09:41 -0700

    returns the length of a string unsing _strlen_recursion

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the charcher c to stdout
+ * _putchar - writes the character c to stdout
  * @c: character to be printed
- * Return: 1 (0Success)
+ * Return: 1 (0 Success)
  * on error, -1 is returned, and errno is set appriopriately
  */
 int_putchar(char c)
